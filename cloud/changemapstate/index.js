33da1eefce6ac16b9c65048477a78a607a1e8b6c
hr99 <1771012486@qq.com>
2020-04-19 23:09:24 +0800

    merge

---
+++ cloud/changemapstate/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database()
+const _ = db.command
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let a = event.id;
+  return await db.collection('maps').doc(a).update({
+    // data 传入需要局部更新的数据
+    data: {
+      isNow: true
+    }
+  })
+
+}
